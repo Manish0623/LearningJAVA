6a4b34b29a73a0cace5c1b2c8aca6bf217f1021b
Manish0623 <manishsingh.jsr2000@gmail.com>
2025-09-07 16:19:38 +0530

    Add files via upload

---
+++ PracticeSet/ReverseArray.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class ReverseArray{
+    public static void main(String[] args) {
+        int[] arr = {10,20,30,40};
+
+        System.out.println("Original Array : " + Arrays.toString(arr));
+
+        reverse(arr);
+
+        System.out.println(" Reverse Array: " + Arrays.toString(arr));
+
+    }
+
+public static void reverse(int[] arr){
+    int start = 0;
+    int end = arr.length - 1;
+
+    while(start < end){
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+
+        start++;
+        end--;
+    }
+}
+
+}
+
