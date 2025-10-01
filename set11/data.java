0187b66288bcf2333e6c8a8e1cb7d989d6350787
Manish0623 <manishsingh.jsr2000@gmail.com>
2025-10-01 23:09:47 +0530

    Add files via upload

---
+++ set11/data.java
@@ -0,0 +1,36 @@
+/*  
+Anatomy of a java program
+
+Documentation Section -- Suggested 
+Package Statement -- optional
+import Statements -- optional
+InterFace statement --optional
+class  statement -- mandatory
+
+main method is mandatory for the execution of the program....
+
+
+Dats types in java 
+
+types
+
+1 . Primitive data types..
+byte,int,char,boolean,float,double,long,short...
+
+2. nom primitive data types..
+
+*/ 
+
+
+
+
+public class data {
+    public static void main(String[] args){
+         int a =10;
+         int b=20;
+         int c= 30;
+
+         int sum = a+b+c;
+         System.out.println("The sum of the numbers are:" + sum);
+    }
+}
