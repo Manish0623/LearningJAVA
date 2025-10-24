a4a570f60539b07ae06586587b47fe624330c3d4
Manish0623 <manishsingh.jsr2000@gmail.com>
2025-10-24 23:11:16 +0530

    Add files via upload

--- set6/starPattern.java
+++ set6/starPattern.java
@@ -6,7 +6,7 @@ int row = 5 ;
 
 for ( int i = 1;  i<=row ; i++){
     for( int j= 1 ; j <= i ; j++){
-        System.out.print("@ ");
+        System.out.print("  ");
     }
    System.out.println(); 
 }
